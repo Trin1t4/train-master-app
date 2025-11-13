4b592208d92083b85dad7f7f0836fe32d116097f
Trin1t4 <dario.tripol@gmail.com>
2025-11-13 09:01:36 +0100

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>TrainMaster</Text>
+      <Text>Workout tracking & recovery optimization!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  heading: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 12
+  }
+});
